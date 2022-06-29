f2ff694e5a134c584a6f531aa5f8071474b856a6
Olga <olga028383@mail.ru>
2022-06-29 13:50:24 +0300

    Added search test

---
+++ src/components/search/search.test.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {Router} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Search from './search';
+import {createApi} from '../../api';
+import {FakeStore, Guitar} from '../../mock/test';
+import {searchGuitars} from '../../store/api-actions';
+
+jest.mock('../../store/api-actions');
+jest.mock('../../store/action');
+
+const mockSearchGuitars = searchGuitars as jest.MockedFunction<typeof searchGuitars>;
+
+const api = createApi();
+const middlewares = [thunk.withExtraArgument(api)];
+const mockStore = configureStore(middlewares);
+
+describe('Component: Search', () => {
+
+  it('should display Search', () => {
+    const history = createMemoryHistory();
+    const store = mockStore(FakeStore);
+
+    const fakeApp = (
+      <Provider store={store}>
+        <Router history={history}>
+          <Search/>;
+        </Router>
+      </Provider>
+    );
+    render(fakeApp);
+
+    expect(screen.getByText(/Начать поиск/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/что вы ищите?/)).toBeInTheDocument();
+  });
+
+  it('should call onChange', async() => {
+    mockSearchGuitars.mockReturnValue(Promise.resolve([Guitar, Guitar]));
+    const history = createMemoryHistory();
+    const store = mockStore(FakeStore);
+
+    const fakeApp = (
+      <Provider store={store}>
+        <Router history={history}>
+          <Search/>;
+        </Router>
+      </Provider>
+    );
+    render(fakeApp);
+
+    const searchInput = screen.getByPlaceholderText(/что вы ищите/);
+    userEvent.type(searchInput, 'Че');
+    expect(await screen.findAllByText(/Честер Bass/i)).toHaveLength(2);
+  });
+});
